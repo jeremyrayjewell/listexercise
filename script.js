cada96c5a115bddd8a050b82fc9b24b4b14b915f
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-09-09 15:16:20 -0400

    e

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-    if (input.length>0) {
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li)
-    input.value = "";
-}
-})
+button.addEventListener("click", function () {
+    if (input.value.length > 0) { // Check the value property for input
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
+});
 
-input.addEventListener("keydown", function(event){
-    if (input.length>0 && event.key === 'Enter') {
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li)
-    input.value = "";
-}
-})
+input.addEventListener("keydown", function (event) {
+    if (input.value.length > 0 && event.key === 'Enter') { // Check the value property and 'Enter' key
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
+});
