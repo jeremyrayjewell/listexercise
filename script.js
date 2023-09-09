8a36b1af10fa23aad64363a2cdfbdba5fe329752
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-09-09 16:35:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -44,13 +44,22 @@ function exportAsPDF() {
  doc.setFontSize(16); // Set font size for the header
  doc.text("To-Do List", 20, 20); // Position and text for the header
 
+ // Add an image
+ const imageURL = "https://avatars.githubusercontent.com/u/140910682?v=4";
+ doc.addImage(imageURL, 'JPEG', 160, 10, 30, 30); // Adjust the position and size of the image
+
+ // Add text below the image
+ doc.setFontSize(8); // Set a smaller font size for the text
+ doc.text("https://github.com/jeremyrayjewell", 155, 50); // Position and text for the URL
+
  // Get all the list items
  const listItems = document.querySelectorAll('ul li');
 
- let y = 40; // Adjust the Y position to start below the header
+ let y = 70; // Adjust the Y position to start below the header and image
 
  listItems.forEach((item) => {
-     const text = item.textContent.trim();
+     // Exclude the text inside the "Remove" buttons
+     const text = item.firstChild.textContent.trim(); // Get only the list item's text
      doc.text(20, y, text);
      y += 10;
  });
