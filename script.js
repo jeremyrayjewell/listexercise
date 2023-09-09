6b6636ffa91b80e5c7deac5d9aadeab2fa84eda1
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-09-09 15:53:49 -0400

    reorganized

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function () {
-    if (input.value.length > 0) { // Check the value property for input
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
-    }
-});
+function inputLength() {
+    return input.value.length;
+}
 
-input.addEventListener("keydown", function (event) {
-    if (input.value.length > 0 && event.key === 'Enter') { // Check the value property and 'Enter' key
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
+function createListElement() {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
     }
-});
+}
+
+function addListAfterKeydown(event){
+    if (inputLength() > 0 && event.key === 'Enter') {
+        createListElement();
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keydown", function (addListAfterKeydown)
