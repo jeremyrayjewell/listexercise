55958c0287acbbf0fc12d73eb420af013b2bc134
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-09-09 16:43:47 -0400

    fs

--- script.js
+++ script.js
@@ -41,30 +41,25 @@ function exportAsPDF() {
     const doc = new jsPDF();
 
  // Add a header
- doc.setFontSize(16); // Set font size for the header
- doc.text("To-Do List", 20, 20); // Position and text for the header
+    doc.setFontSize(16); // Set font size for the header
+    doc.text("To-Do List", 20, 20); // Position and text for the header
 
- // Add an image
- const imageURL = "https://avatars.githubusercontent.com/u/140910682?v=4";
- doc.addImage(imageURL, 'JPEG', 160, 10, 30, 30); // Adjust the position and size of the image
+    // Get all the list items
+    const listItems = document.querySelectorAll('ul li');
 
- // Add text below the image
- doc.setFontSize(8); // Set a smaller font size for the text
- doc.text("https://github.com/jeremyrayjewell", 155, 50); // Position and text for the URL
+    let y = 70; // Adjust the Y position to start below the header and image
 
- // Get all the list items
- const listItems = document.querySelectorAll('ul li');
+    listItems.forEach((item) => {
+        // Exclude the text inside the "Remove" buttons
+        const text = item.firstChild.textContent.trim(); // Get only the list item's text
+        doc.text(20, y, text);
+        y += 10;
+    });
 
- let y = 70; // Adjust the Y position to start below the header and image
+    doc.setFontSize(8); // Set a smaller font size for the text
+    doc.text("https://github.com/jeremyrayjewell", 155, 5); // Position and text for the URL
 
- listItems.forEach((item) => {
-     // Exclude the text inside the "Remove" buttons
-     const text = item.firstChild.textContent.trim(); // Get only the list item's text
-     doc.text(20, y, text);
-     y += 10;
- });
-
- doc.save('todo-list.pdf');
+    doc.save('todo-list.pdf');
 }
 
 // Add an event listener for the export button
