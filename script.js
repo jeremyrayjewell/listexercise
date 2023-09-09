eca73b052787b9bc1cdea5985c2d810cc0295d46
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-09-09 15:14:43 -0400

    input event listener

--- script.js
+++ script.js
@@ -6,5 +6,16 @@ button.addEventListener("click", function(){
     if (input.length>0) {
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li)}
+    ul.appendChild(li)
+    input.value = "";
 }
+})
+
+input.addEventListener("keydown", function(event){
+    if (input.length>0 && event.key === 'Enter') {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li)
+    input.value = "";
+}
+})
